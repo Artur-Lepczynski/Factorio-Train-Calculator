4c93aedb4302781026303386225c956ba039c035
Artur-Lepczynski <artur.lepczynski@gmail.com>
2024-02-01 23:50:28 +0100

    started work on Form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,5 +1,26 @@
-import style from "./Form.module.css"
+import { useState } from "react";
+import style from "./Form.module.css";
 
-export default function Form(){
-  return <p>Form</p>
+export default function Form() {
+  const [target, setTarget] = useState<number>(1);
+
+  function handleTargetChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTarget(+event.target.value); 
+  }
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style["form-item"]}>
+        <label htmlFor="targetInput">Target</label>
+        <input
+          id="targetInput"
+          type="number"
+          min="1"
+          max="1000"
+          value={target}
+          onChange={handleTargetChange}
+        ></input>
+      </div>
+    </div>
+  );
 }
